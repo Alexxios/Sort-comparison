6a8d933fb2ceb49306288cbe12547db7a91e493d
Alexxios <aglotov@ithse.ru>
2023-11-01 02:12:29 +0300

    completed main

--- sources/main.cpp
+++ sources/main.cpp
@@ -20,36 +20,34 @@ Func *const func_array<Func>::data[] = { insert_sort, merge_sort };
 
 
 int main() {
-    size_t testn[] = {5, 10, 15, 20, 30, 45, 60, 100, 500, 1000, 2000, 5000, 15000, 25000};
+    size_t testn[] = {5, 10, 15, 20, 30, 45, 60, 80, 100, 500, 1000, 2000, 5000};
     vector<int> a;
     mt19937 gen(time(nullptr));
     high_resolution_clock hrc;
 
     ofstream fout;
     fout.open("output.txt");
+    fout << "N;insert_sort;merge_sort" << endl;
 
     // T - number of runs for each array length
-    #define T 20
+    #define T 100
 
     for (auto& N : testn) {
         a.resize(N);
 
-        double duration_sum[] = {0, 0};
         for (size_t run = 0; run != T; ++run) {
             for (auto& x : a) x = gen();
 
-            size_t i = 0;
+            fout << N;
             for (auto f : func_array<void(vector<int>&)>::data) {
                 auto start = hrc.now();
                 f(a);
                 auto stop = hrc.now();
 
-                duration_sum[i++] += duration<double, milli>(stop - start).count();
+                fout << ';' << fixed << duration<double, milli>(stop - start).count();
             }
+            fout << endl;
         }
-        fout << N;
-        for (double d : duration_sum) fout << ',' << fixed << d / N;
-        fout << endl;
     }
     #undef T
     
